276370c7fe25c3c61cd20ae6db845b5e8d90c575
lokomedio89 <lokomedio89@gmail.com>
2024-10-11 10:40:21 +0200

    viernes

--- ConectarMySQL.java
+++ ConectarMySQL.java
@@ -1,4 +1,4 @@
-public class ConectarMySQL {
+public class ConectarMySQL {//comentari de Pedro
     public static void main(String[] args) {
         // Cargar el controlador JDBC de MySQL
         try {
